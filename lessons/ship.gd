e85c3cac118bdf7d636252c3251326318f9b4c52
wiinngg <wikuang2@s.sfusd.edu>
2025-12-04 13:35:48 -0800

    M5L6

--- lessons/ship.gd
+++ lessons/ship.gd
@@ -5,6 +5,7 @@ var velocity := Vector2(0, 0)
 var steering_factor := 3.0
 
 var health := 10
+var gem_count := 0
 
 
 func _ready() -> void:
@@ -29,10 +30,18 @@ func _process(delta: float) -> void:
 		get_node("Sprite2D").rotation = velocity.angle()
 
 
+func set_gem_count(new_gem_count: int) -> void:
+	gem_count = new_gem_count
+	get_node("UI/GemCount").text = "x" + str(gem_count)
+
+
 func set_health(new_health: int) -> void:
 	health = new_health
 	get_node("UI/HealthBar").value = health
 
 
 func _on_area_entered(area_that_entered: Area2D) -> void:
-	set_health(health + 10)
+	if area_that_entered.is_in_group("gem"):
+		set_gem_count(gem_count + 1)
+	elif area_that_entered.is_in_group("healing_item"):
+		set_health(health + 10)
